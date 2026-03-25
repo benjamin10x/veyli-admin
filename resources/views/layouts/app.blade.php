c72febe8619cd3752cc6e38bc6ff5f8dcc161b3e
benjamin10x <benjamoraleshdz@gmail.com>
2026-03-25 08:33:38 -0600

    livewire

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,6 +7,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet">
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet">
     @vite(['resources/css/app.css', 'resources/js/app.js', 'resources/images/logo.png'])
+    @livewireStyles
 </head>
 <body>
     <div class="app-container">
@@ -68,5 +69,8 @@
             });
         }
     </script>
+    {{ $slot ?? '' }}
+
+    @livewireScripts
 </body>
 </html>
--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -5,10 +5,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'Login')</title>
     @vite(['resources/css/app.css', 'resources/js/app.js', 'resources/images/logo.png'])
+    @livewireStyles
 </head>
 <body>
     <main>
         @yield('content')
     </main>
+    {{ $slot ?? '' }}
+
+    @livewireScripts
 </body>
 </html>
